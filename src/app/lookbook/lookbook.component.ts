d3058a9670c206e007638ca8080a8b4492d2047b
paferuto <haodh20411@st.uel.edu.vn>
2023-05-12 11:14:36 +0700

    lookbook

--- src/app/lookbook/lookbook.component.ts
+++ src/app/lookbook/lookbook.component.ts
@@ -16,11 +16,12 @@ export class LookbookComponent {
   id: any;
   collection: any;
   products: any[] = [];
-  listProducts: any;
+  listProducts: any[]= [];
   target: any = "";
   videoURL: any;
   lookbook: any;
   index:any;
+  shownIndex: number = 0;
   constructor(private _service: CollectionService, private route: ActivatedRoute, private router: Router, private _title: Title, public _format: FormatService, private productService: ProductService, private sanitizer: DomSanitizer) {
     this._title.setTitle("Collection");
     const urlParams = new URLSearchParams(window.location.search);
@@ -28,16 +29,13 @@ export class LookbookComponent {
     this.index = urlParams.get('index');
     this._service.getCollectionById(this.id).subscribe(
       (data: any) => {
-        this.collection = data;
+        this.collection = new Collection(data);
         this.lookbook = data.lookbook[this.index]
+        this.getProductsByLookbook(this.lookbook);
+        this.shownIndex = parseInt(this.index) + 1;
       },
       (error) => {
         console.log(error);
-      }
-    );
-    this.productService.getAllProducts().subscribe(
-      (data: any) => {
-        this.listProducts = data.slice(0, 5);
       }
     );
   }
@@ -51,5 +49,38 @@ export class LookbookComponent {
         console.log(error);
       }
     );
+  }
+
+  getProductsByLookbook(lookbook: any) {
+    for (let i = 0; i < lookbook.products.length; i++) {
+      this.productService.getProductById(lookbook.products[i]).subscribe(
+        (data: any) => {
+          this.listProducts.push(data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  nextLookbook() {
+    if (this.index < this.collection.lookbook.length - 1) {
+      this.index++;
+      this.lookbook = this.collection.lookbook[this.index];
+      this.listProducts = [];
+      this.getProductsByLookbook(this.lookbook);
+      this.shownIndex = this.index + 1;
+    }
+  }
+
+  previousLookbook() {
+    if (this.index > 0) {
+      this.index--;
+      this.lookbook = this.collection.lookbook[this.index];
+      this.listProducts = [];
+      this.getProductsByLookbook(this.lookbook);
+      this.shownIndex = this.index + 1;
+    }
   }
 }
