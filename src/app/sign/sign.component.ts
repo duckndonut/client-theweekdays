25cc5affe9aefba27972d2b9c5d5b38c4bdcd039
Thanh <thanhenc@gmail.com>
2023-05-12 21:35:38 +0700

    auth

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/service/auth.service';
+import { FormatService } from 'src/service/format.service';
 
 @Component({
   selector: 'app-sign',
@@ -6,5 +10,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign.component.css']
 })
 export class SignComponent {
+  account = {
+    username: '',
+    password: ''
+  };
 
+  signup_account = {
+    email: '',
+    phone: '',
+    username: '',
+    password: ''
+  };
+
+  ui = this._format.useLanguage();
+
+  constructor(public _format: FormatService, private _auth: AuthService, private _toastr: ToastrService, private _router: Router) {
+    this._auth.hasLoggedIn().subscribe({
+      next: (res) => {
+        if (res) {
+          this._toastr.info(this.ui.already_login_message);
+          this._router.navigate(['/']);
+        }
+      }
+    });
+  }
+
+  // login
+  login(account_info: any) {
+    this._auth.login(account_info).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.accessToken);
+        localStorage.setItem('refresh_token', res.refreshToken);
+        this._toastr.success(this.ui.success_login_message);
+        this._router.navigate(['/']);
+      },
+      error: (err) => {
+        this._toastr.error(this.ui.fail_login_message, this.ui.fail_login);
+      }
+    });
+  }
+
+  // signup
+  signup(account_info: any) {
+    this._auth.signup(account_info).subscribe({
+      next: (res) => {
+        this._toastr.success(this.ui.success_register_message);
+        this.resetSignupForm();
+      },
+      error: (err) => {
+        this._toastr.error(this.ui.fail_signup_message, this.ui.fail_signup);
+      }
+    });
+  }
+
+  // reset signup form
+  resetSignupForm() {
+    this.signup_account = {
+      email: '',
+      phone: '',
+      username: '',
+      password: ''
+    };
+  }
 }
